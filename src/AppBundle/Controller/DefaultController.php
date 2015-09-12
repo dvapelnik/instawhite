990ecead7a1c41d601d31b8d71d8ec9942c20b0d
Dmitry Vapelnik <dvapelnik@gmail.com>
2015-09-12 14:49:28 +0300

    Login logout wit instagram

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use Guzzle\Service\Client;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -52,6 +53,9 @@ class DefaultController extends Controller
 
         $this->get('session')->set('is_logged', false);
 
+        $this->get('session')->set('instagram', array());
+
+
         return $this->redirectToRoute('homepage');
     }
 
@@ -65,5 +69,28 @@ class DefaultController extends Controller
         if ($request->get('error')) {
             return $this->redirectToRoute('login');
         }
+
+        $client = new Client('https://api.instagram.com/oauth/access_token');
+
+        $request = $client->post()
+            ->setPostField('client_id', $this->getParameter('instagram.client_id'))
+            ->setPostField('client_secret', $this->getParameter('instagram.client_secret'))
+            ->setPostField('grant_type', 'authorization_code')
+            ->setPostField('redirect_uri', $this->generateUrl('instagram_redirect', array(), true))
+            ->setPostField('code', $code);
+        try {
+            $response = $request->send();
+        } catch (\Exception $e) {
+            $this->addFlash('instagram_error', $e->getMessage());
+
+            return $this->redirectToRoute('login');
+        }
+
+        $responseBodyArray = json_decode($response->getBody(true));
+
+        $this->get('session')->set('instagram', $responseBodyArray);
+        $this->get('session')->set('is_logged', true);
+
+        return $this->redirectToRoute('homepage');
     }
 }
